61727d034838cf98067c39c93e3524a3f0eb3196
Nicolas Bock <nicolasbock@gmail.com>
2024-08-14 18:01:26 -0600

    Add linting to CI
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/pick_files.py
+++ scripts/pick_files.py
@@ -1,18 +1,34 @@
+"""
+Pick random files to generate training set.
+"""
+
 import argparse
 import os
 import random
 import shutil
 
 
-def pick_files(directories, num_files, destination):
+def pick_files(directories: list[str], num_files: int, destination: str):
+    """
+    Pick num_files random files from directories and copy files to destination.
+
+    Args:
+        directories (list[str]): A list of directories containing files
+        num_files (int): The number of files to copy
+        destination (str): The destination directory
+
+    Raises:
+        FileNotFoundError: A directory was not found
+    """
     os.makedirs(destination, exist_ok=True)
     files = []
     for directory in directories:
         try:
             files.extend([os.path.join(directory, f)
                           for f in os.listdir(directory)])
-        except FileNotFoundError as e:
-            raise e
+        except FileNotFoundError:
+            print(f'could not open directory {directory}')
+            raise
 
     random.shuffle(files)
     selected_files = files[:num_files]
@@ -20,7 +36,14 @@ def pick_files(directories, num_files, destination):
         shutil.copy2(file, destination)
 
 
-def parse_commandline():
+def parse_commandline() -> argparse.Namespace:
+    """
+    Parse the commandline.
+
+    Returns:
+        argparse.Namespace: The parsed options
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--num-files",
@@ -46,5 +69,9 @@ def parse_commandline():
 
 
 def main():
+    """
+    The entry point.
+    """
+
     options = parse_commandline()
     pick_files(options.source, options.num_files, options.destination)
