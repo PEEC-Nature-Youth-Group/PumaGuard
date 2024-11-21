7b9a1dff63c370c02c0cfa614ca0ab2d7594859f
Nicolas Bock <nicolasbock@gmail.com>
2024-11-21 06:57:05 -0700

    Add scripts to launch without poetry
    On the colab instance we already have all of the necessary Python
    packages installed and dno't need poetry. These scripts allow us to run
    the applications directly.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

---
+++ scripts/classify.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Convenience script to run the classification.
+"""
+
+import sys
+from pumaguard.cmd.classify import main
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ scripts/train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Convenience script to run the training.
+"""
+
+import sys
+from pumaguard.cmd.train import main
+
+if __name__ == '__main__':
+    sys.exit(main())
