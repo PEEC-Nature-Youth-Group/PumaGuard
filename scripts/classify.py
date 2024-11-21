c76b80841b325a688508a310322ede8e08867e8d
Nicolas Bock <nicolasbock@gmail.com>
2024-11-21 08:30:53 -0700

    Fix convenience scripts
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/classify.py
+++ scripts/classify.py
@@ -5,6 +5,11 @@ Convenience script to run the classification.
 """
 
 import sys
+import os.path
+
+# pylint: disable=wrong-import-position
+sys.path.append(os.path.realpath(f'{os.path.dirname(__file__)}/..'))
+
 from pumaguard.cmd.classify import main
 
 if __name__ == '__main__':
--- scripts/train.py
+++ scripts/train.py
@@ -5,6 +5,11 @@ Convenience script to run the training.
 """
 
 import sys
+import os.path
+
+# pylint: disable=wrong-import-position
+sys.path.append(os.path.realpath(f'{os.path.dirname(__file__)}/..'))
+
 from pumaguard.cmd.train import main
 
 if __name__ == '__main__':
