93ad35f5d0a3a1f43ca2dce309892e1a1f6288a6
Nicolas Bock <nicolasbock@gmail.com>
2025-01-06 11:06:19 -0700

    Simplify classify script
    Do not create a whole dataset, but rather load each image individually.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- pumaguard/classify.py
+++ pumaguard/classify.py
@@ -6,20 +6,13 @@ This script classifies images.
 
 import argparse
 import datetime
-import os
-import shutil
 import sys
-import tempfile
 
 import numpy as np
-import keras  # type: ignore
+import keras # type: ignore
+from PIL import Image
 
 from pumaguard.presets import Presets
-from pumaguard.traininghistory import TrainingHistory
-from pumaguard.utils import (
-    create_model,
-    initialize_tensorflow,
-)
 
 
 def get_duration(start_time: datetime.datetime,
@@ -40,64 +33,32 @@ def get_duration(start_time: datetime.datetime,
 
 def classify_images(presets: Presets, model: keras.Model, image_path: str):
     """
-    Classify the images and print out the result.
+    Classify the image and print out the result.
     """
-    if presets.model_version == 'pre-trained':
-        color_model = 'rgb'
-    else:
-        color_model = 'grayscale'
-    print(f'Using color_mode \'{color_model}\'')
-
-    print(f'Loading images from {image_path}')
+    print(f'Using color_mode \'{presets.color_mode}\'')
+    print(f'Classifying image {image_path}')
     start_time = datetime.datetime.now()
-    verification_dataset = keras.preprocessing.image_dataset_from_directory(
-        image_path,
-        batch_size=presets.batch_size,
-        image_size=presets.image_dimensions,
-        shuffle=True,
-        color_mode=color_model,
-    )
+    if presets.color_mode == 'rgb':
+        img = Image.open(image_path).convert('RGB')
+    else:
+        img = Image.open(image_path).convert('L')
+    img = img.resize(presets.image_dimensions)
+
+    img_array = np.array(img)
+    # img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    if presets.color_mode == 'grayscale':
+        img_array = np.expand_dims(img_array, axis=-1)
+
+    prediction = model.predict(img_array)
+
     end_time = datetime.datetime.now()
-    print(f'Loaded images, {get_duration(start_time, end_time)} seconds')
+    print('Classification took '
+          f'{get_duration(start_time, end_time)} seconds')
 
-    all_predictions = []
-    all_labels = []
-
-    for images, labels in verification_dataset:
-        print('Starting classification of batch '
-              f'of {presets.batch_size} images')
-        start_time = datetime.datetime.now()
-        predictions = model.predict(images)
-        end_time = datetime.datetime.now()
-        print('Classification took '
-              f'{get_duration(start_time, end_time)} seconds')
-        all_predictions.extend(predictions)
-        all_labels.extend(labels)
-
-    tmp = [a.tolist() for a in all_predictions]
-    tmp2 = []
-    for a in tmp:
-        tmp2.extend(a)
-    all_predictions = tmp2
-
-    tmp = [a.numpy().tolist() for a in all_labels]
-    all_labels = tmp
-
-    # Calculate the percentage of correctly classified images
-    correct_predictions = np.sum(np.round(all_predictions) == all_labels)
-    total_images = len(all_labels)
-    accuracy = correct_predictions / total_images * 100
-
-    print(f"Percentage of correctly classified images: {accuracy:.2f}%")
-
-    for images, labels in iter(verification_dataset):
-        # Predict the labels for the images
-        predictions = model.predict(images)
-        for i in range(len(images)):
-            print(
-                f'Predicted: {100*(1 - predictions[i][0]):6.2f}% lion '
-                f'({"lion" if predictions[i][0] < 0.5 else "no lion"}), '
-                f'Actual: {"lion" if labels[i] == 0 else "no lion"}')
+    print(
+        f'Predicted: {100*(1 - prediction[0][0]):6.2f}% lion '
+        f'({"lion" if prediction[0][0] < 0.5 else "no lion"})')
 
 
 def parse_commandline() -> argparse.Namespace:
@@ -116,9 +77,8 @@ def parse_commandline() -> argparse.Namespace:
     )
     parser.add_argument(
         'image',
-        metavar='FILE[:{lion,no-lion}]',
-        help=('An image to classify with an optional label. '
-              'If the label is missing then "lion" is assumed.'),
+        metavar='FILE',
+        help='An image to classify.',
         nargs='*',
         type=str,
     )
@@ -150,7 +110,7 @@ _pumaguard_classify_completions() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h --help --notebook"
+    opts="-h --help --notebook --completion"
 
     if [[ ${cur} == -* ]]; then
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
@@ -161,7 +121,12 @@ _pumaguard_classify_completions() {
         --notebook)
             return 0
             ;;
-        *)
+       --completion)
+            opts="bash"
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+         *)
             COMPREPLY=( $(compgen -f -- ${cur}) )
             return 0
             ;;
@@ -176,69 +141,13 @@ complete -F _pumaguard_classify_completions pumaguard-classify
 ''')
 
 
-def split_argument(image: str) -> tuple[str, str]:
-    """
-    Split a FILE:[{lion,no-lion}] command line argument into filename and
-    label.
-
-    Arguments:
-        image -- The FILE:[{lion.no-lion}] command line argument.
-
-    Raises:
-        ValueError: If an unknown syntax was used for the argument.
-
-    Returns:
-        A (filename, label) tuple.
-    """
-    parsed = image.split(':')
-    if len(parsed) == 1:
-        filename = parsed[0]
-        label = 'lion'
-    elif len(parsed) == 2:
-        filename, label = parsed
-        if label not in ['lion', 'no-lion']:
-            raise ValueError(
-                f'unknown category {label}'
-            )
-    else:
-        raise ValueError(
-            'please use FILE[:{lion,no-lion}]'
-        )
-    return (filename, label)
-
-
 def main():
     """
     Main entry point
     """
 
     options = parse_commandline()
-
-    with tempfile.TemporaryDirectory() as workdir:
-        images = {}
-        for image in options.image:
-            filename, label = split_argument(image)
-            if label not in images:
-                images[label] = []
-            images[label].append(filename)
-
-        for label in images:  # pylint: disable=consider-using-dict-items
-            os.makedirs(os.path.join(workdir, label))
-            for filename in images[label]:
-                shutil.copy(filename, os.path.join(workdir, label))
-
-        presets = Presets(options.notebook)
-        full_history = TrainingHistory(presets)
-
-        best_accuracy, best_val_accuracy, \
-            best_loss, best_val_loss, best_epoch = \
-            full_history.get_best_epoch('accuracy')
-        print(f'Total time {sum(full_history.history["duration"])} '
-              f'for {len(full_history.history["accuracy"])} epochs')
-        print(f'Best epoch {best_epoch} - accuracy: {best_accuracy:.4f} - '
-              f'val_accuracy: {best_val_accuracy:.4f} - loss: {best_loss:.4f} '
-              f'- val_loss: {best_val_loss:.4f}')
-
-        distribution_strategy = initialize_tensorflow()
-        model = create_model(presets, distribution_strategy)
-        classify_images(presets, model, workdir)
+    presets = Presets(options.notebook)
+    model = keras.models.load_model(presets.model_file)
+    for image in options.image:
+        classify_images(presets, model, image)
