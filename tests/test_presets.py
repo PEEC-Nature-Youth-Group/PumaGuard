78acefe61719bd8dc22b74c385b9182fa51d947c
Nicolas Bock <nicolasbock@gmail.com>
2025-01-15 16:14:03 -0700

    Introduce BasePreset class
    This will eventually be refactored so that the presets are not hardcoded
    in a large if/elif/else tree.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- tests/test_presets.py
+++ tests/test_presets.py
@@ -6,6 +6,7 @@ import os
 import unittest
 
 from pumaguard.presets import (
+    BasePreset,
     Presets,
 )
 
@@ -53,8 +54,8 @@ class TestPresets(unittest.TestCase):
         Test the default no_lion_directories for a known notebook_number.
         """
         self.assertIn(
-                os.path.join(self.presets.base_data_directory, 'no_lion'),
-                self.presets.no_lion_directories)
+            os.path.join(self.presets.base_data_directory, 'no_lion'),
+            self.presets.no_lion_directories)
 
     def test_no_lion_directories_getter_base(self):
         """
@@ -79,3 +80,49 @@ class TestPresets(unittest.TestCase):
             for no_lion in custom_no_lions
         ]
         self.assertListEqual(self.presets.no_lion_directories, expected_paths)
+
+    def test_notebook_number_getter_default(self):
+        """
+        Test whether the default notebook number is correcct.
+        """
+        presets = Presets(1)
+        self.assertEqual(presets.notebook_number, 1)
+
+    def test_notebook_number_setter(self):
+        """
+        Test whether one can set a notebook number.
+        """
+        presets = Presets(1)
+        with self.assertRaises(ValueError) as e:
+            presets.notebook_number = 0
+        self.assertEqual(str(e.exception),
+                         'notebook can not be zero or negative (0)')
+
+
+class TestBasePreset(unittest.TestCase):
+    """
+    Test the base class.
+    """
+
+    def setUp(self):
+        self.base_preset = BasePreset()
+
+    def test_image_dimensions_default(self):
+        """
+        Test the default value of image dimensions.
+        """
+        self.assertEqual(len(self.base_preset.image_dimensions), 2)
+        self.assertEqual(self.base_preset.image_dimensions, (128, 128))
+
+    def test_image_dimensions_failure(self):
+        """
+        Test various failures of image dimensions.
+        """
+        with self.assertRaises(TypeError) as e:
+            self.base_preset.image_dimensions = 1
+        self.assertEqual(str(e.exception),
+                         'image dimensions needs to be a tuple')
+        with self.assertRaises(ValueError) as e:
+            self.base_preset.image_dimensions = (-1, 2)
+        self.assertEqual(str(e.exception),
+                         'image dimensions need to be positive')
