ca5d10ef93343f0177389f31253c86c0c22e7fc5
Nicolas Bock <nicolasbock@gmail.com>
2025-01-21 01:37:31 +0000

    Indent bash scripts
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- pumaguard/completions/pumaguard-classify-completions.sh
+++ pumaguard/completions/pumaguard-classify-completions.sh
@@ -5,7 +5,7 @@ _pumaguard_classify_completions() {
 
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    prev="${COMP_WORDS[COMP_CWORD - 1]}"
     opts=(
         -h
         --help
@@ -17,37 +17,37 @@ _pumaguard_classify_completions() {
     )
 
     if [[ ${cur} == -* ]]; then
-        COMPREPLY=( $(compgen -W "${opts[*]}" -- "${cur}") )
+        COMPREPLY=($(compgen -W "${opts[*]}" -- "${cur}"))
         return 0
     fi
 
     case "${prev}" in
-        --completion)
-            opts=(bash)
-            COMPREPLY=( $(compgen -W "${opts[*]}" -- "${cur}") )
-            return 0
-            ;;
-        --notebook)
-            return 0
-            ;;
-        --model-path)
-            COMPREPLY=( $(compgen -d -o dirnames -o nospace -- "${cur}") )
-            return 0
-            ;;
-        *)
-            # Suggest files, but avoid the trailing space for directories
-            if [[ "${cur}" == */ ]]; then
-                # Only suggest directories if the input ends with a slash
-                COMPREPLY=( $(compgen -d -- "${cur}") )
-            else
-                # Suggest files and directories without a trailing space
-                COMPREPLY=( $(compgen -f -- "${cur}") )
-            fi
-            return 0
-            ;;
+    --completion)
+        opts=(bash)
+        COMPREPLY=($(compgen -W "${opts[*]}" -- "${cur}"))
+        return 0
+        ;;
+    --notebook)
+        return 0
+        ;;
+    --model-path)
+        COMPREPLY=($(compgen -d -o dirnames -o nospace -- "${cur}"))
+        return 0
+        ;;
+    *)
+        # Suggest files, but avoid the trailing space for directories
+        if [[ "${cur}" == */ ]]; then
+            # Only suggest directories if the input ends with a slash
+            COMPREPLY=($(compgen -d -- "${cur}"))
+        else
+            # Suggest files and directories without a trailing space
+            COMPREPLY=($(compgen -f -- "${cur}"))
+        fi
+        return 0
+        ;;
     esac
 
-    COMPREPLY=( $(compgen -W "${opts[*]}" -- "${cur}") )
+    COMPREPLY=($(compgen -W "${opts[*]}" -- "${cur}"))
     return 0
 }
 
--- pumaguard/completions/pumaguard-server-completions.sh
+++ pumaguard/completions/pumaguard-server-completions.sh
@@ -5,40 +5,47 @@ _pumaguard_server_completions() {
 
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts="-h --help --debug --notebook --watch-method \
-        --model-path --completion --settings"
-
+    prev="${COMP_WORDS[COMP_CWORD - 1]}"
+    opts=(
+        -h
+        --help
+        --debug
+        --notebook
+        --watch-method
+        --model-path
+        --completion
+        --settings
+    )
     if [[ ${cur} == -* ]]; then
-        COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
         return 0
     fi
 
     case "${prev}" in
-        --notebook)
-            return 0
-            ;;
-        --model-path)
-            COMPREPLY=( $(compgen -d -o dirnames -o nospace -- "${cur}") )
-            return 0
-            ;;
-        --watch-method)
-            opts="inotify os"
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-            return 0
-            ;;
-        --completion)
-            opts="bash"
-            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
-            return 0
-            ;;
-        *)
-            COMPREPLY=( $(compgen -d -- "${cur}") )
-            return 0
-            ;;
+    --notebook)
+        return 0
+        ;;
+    --model-path)
+        COMPREPLY=($(compgen -d -o dirnames -o nospace -- "${cur}"))
+        return 0
+        ;;
+    --watch-method)
+        opts="inotify os"
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+        return 0
+        ;;
+    --completion)
+        opts="bash"
+        COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
+        return 0
+        ;;
+    *)
+        COMPREPLY=($(compgen -d -- "${cur}"))
+        return 0
+        ;;
     esac
 
-    COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+    COMPREPLY=($(compgen -W "${opts}" -- "${cur}"))
     return 0
 }
 
