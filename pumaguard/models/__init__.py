2c1a1cebdf8811821f414d43da08c4c6aaabcdeb
Nicolas Bock <nicolasbock@gmail.com>
2025-01-31 17:32:24 -0700

    Add another ultra light model
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- pumaguard/models/__init__.py
+++ pumaguard/models/__init__.py
@@ -6,6 +6,7 @@ from pumaguard.models import (
     light,
     light_2,
     light_3,
+    light_4,
     pretrained,
 )
 
@@ -14,4 +15,5 @@ __MODEL_FUNCTIONS__ = {
     'light-model': light.light_model,
     'light-2-model': light_2.light_model_2,
     'light-3-model': light_3.light_model_3,
+    'light-4-model': light_4.light_model_4,
 }
